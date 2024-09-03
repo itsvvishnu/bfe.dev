cffc071377e122c5a0c7c8e80994ef0f973d1116
Vishnu V <vishnu@Atom.local>
2024-09-03 19:23:27 +0530

    first commit

---
+++ src/hooks/useTimer.tsx
@@ -0,0 +1,10 @@
+export function useTimeout(callback: () => void, delay: number) {
+    // your code here
+  }
+  
+  // if you want to try your code on the right panel
+  // remember to export App() component like below
+  
+  // export function App() {
+  //   return <div>your app</div>
+  // }
